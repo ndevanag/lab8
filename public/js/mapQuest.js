35f101b27fd5d267aa9580a7a23716ac91dfa118
ndevanag <ndevanag@ucsd.edu>
2021-02-25 10:56:54 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'buPsHWjQ5rMsVuKdwbwDn4z5n62Hab2x';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [37.7749, -122.4194],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12
+	});
+
+	L.marker([37.7749, -122.4194]).addTo(map);
 }
